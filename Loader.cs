7d8a1b4b896f536e904d9a4ab73b7b9f6e02ca1e
Erenn <60568960+sadece1eren@users.noreply.github.com>
2026-02-08 23:33:57 +0300

    Create Loader.cs

---
+++ Loader.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+namespace YapYapTrainer
+{
+    public class Loader
+    {
+        public static void Init()
+        {
+            Loader.Load = new UnityEngine.GameObject();
+            Loader.Load.AddComponent<Cheat>();
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+        public static void UnLoad()
+        {
+            _Unload();
+        }
+        private static void _Unload()
+        {
+            GameObject.Destroy(Load);
+        }
+        private static GameObject Load;
+    }
+}
